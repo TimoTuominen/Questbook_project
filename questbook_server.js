3636ae88be7b1d5a4b2c7cf306ce8dc1c30ae962
Timo <timotuominen111@hotmail.com>
2022-03-10 22:10:10 +0200

    1

--- questbook_server.js
+++ questbook_server.js
@@ -1,13 +1,28 @@
 var http = require("http");
+var fs = require("fs");
+var url = require("url");
+
 var server = http.createServer(function (request, response) {
   if (request.url === "/") {
     response.writeHead(200, { "Content-Type": "text/plain" });
     response.end("Hello hello... sivu1");
   }
 
-  if (request.url === "/own2") {
+  if (request.url === "/guestbook") {
+    fs.readFile(`${__dirname}/questdata.json`, "utf-8", (err, data) => {
+      const vieraskirja = JSON.parse(data);
+      console.log(vieraskirja);
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(data);
+    });
+  }
+  if (request.url === "/newmessage") {
     response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello hello... sivu2");
+    response.end("Hello hello... sivu3");
+  }
+  if (request.url === "/ajaxmessage") {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("Hello hello... sivu4");
   }
 });
 
