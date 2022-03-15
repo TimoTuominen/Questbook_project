5b878cd86d0556b2c7b14a5a90deabc4bbe75d04
Timo <timotuominen111@hotmail.com>
2022-03-15 03:31:55 +0200

    1

--- questbook_server.js
+++ questbook_server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 const app = express();
-var path = require("path");
+//var path = require("path");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/static", express.static("static"));
 
 const replaceTemplate = (temp, guestdata) => {
@@ -13,6 +15,10 @@ const replaceTemplate = (temp, guestdata) => {
   return output;
 };
 
+//let d = new Date(year, month, day);
+
+var utc = new Date().toJSON().slice(0, 10).replace(/-/g, "/");
+
 const guestinfo = fs.readFileSync(`${__dirname}/guestinfo.html`, "utf8");
 const guestbook = fs.readFileSync(`${__dirname}/guestbook.html`, "utf8");
 
@@ -20,6 +26,7 @@ const data = fs.readFileSync(`${__dirname}/questdata.json`, "utf8");
 let guestdata = JSON.parse(data);
 let guesthtml = guestdata.map((el) => replaceTemplate(guestinfo, el)).join("");
 const output = guestbook.replace("%guesttemplate%", guesthtml);
+let guestlenght = guestdata.length;
 
 app.get("/", (req, res) => {
   res.status(200).sendFile(__dirname + "/index.html");
@@ -31,6 +38,23 @@ app.get("/guestbook", (req, res) => {
 
 app.get("/newmessage", (req, res) => {
   res.status(200).sendFile(__dirname + "/newmessage.html");
+});
+
+app.post("/newmessage", (req, res) => {
+  let guestdata2 = {
+    id: guestlenght + 1,
+    username: req.body.name,
+    country: req.body.country,
+    date: utc,
+    message: req.body.message,
+  };
+  console.log(guestdata2);
+  let data1 = JSON.stringify(guestdata2, null, 2);
+  fs.writeFile("questdata.json", data1, (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+  //res.status(200).sendFile(__dirname + "/newmessage.html");
 });
 
 app.get("/ajaxmessage", (req, res) => {
